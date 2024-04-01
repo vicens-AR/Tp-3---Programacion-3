e44878ca45ad40e62c908bf1fd82fa45bda572b5
unknown <d47797531@alumnos.itr.edu.ar>
2024-04-01 20:10:01 -0300

    act 2 - facil empezada

---
+++ ACT2/ACT 2 - FACIL.py
@@ -0,0 +1,6 @@
+frutas_encontradas = ('manzana', 'pera', 'banana', 'durazno', 'sandia')
+
+# Mostrar el contenido de la tupla
+print("Las frutas encontradas son:")
+for fruta in frutas_encontradas:
+    print(fruta)
